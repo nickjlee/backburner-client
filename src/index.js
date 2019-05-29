0c7acd5337a7e865807d8e7c6dd2a3b39a2beb18
Nick Lee <leexnick@gmail.com>
2019-05-29 10:08:43 -0700

    add context to TaskListPage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { TaskListProvider } from './contexts/TaskListContext'
 import App from './components/App/App'
 import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <BrowserRouter>
+    <TaskListProvider>
+      <App />
+    </TaskListProvider>
+  </BrowserRouter>,
+  document.getElementById('root')
+)
--- src/routes/TaskListPage/TaskListPage.js
+++ src/routes/TaskListPage/TaskListPage.js
@@ -1,20 +1,34 @@
 import React, { Component } from 'react'
+import TaskListContext from '../../contexts/TaskListContext'
 import TaskListItem from '../../components/TaskListItem/TaskListItem'
 
 import store from '../../dummy-store'
 
 export default class TaskListPage extends Component {
-  state = { hasError: false }
+  static contextType = TaskListContext
+
+  componentDidMount() {
+    this.context.clearError()
+    // api-service call here to get taskList
+    this.context.setTaskList(store)
+  }
 
   renderTasks() {
-    return store.map(task => <TaskListItem key={task.id} task={task} />)
+    const { taskList = [] } = this.context
+
+    return taskList.map(task => <TaskListItem key={task.id} task={task} />)
   }
 
   render() {
+    const { error } = this.context
+
     return (
-      <>
-        {this.renderTasks()}
-      </>
+      <section className="TaskListPage">
+        {error
+          ? <p className='red'>There was an error, please try again</p>
+          : this.renderTasks()
+        }
+      </section>
     )
   }
 }
