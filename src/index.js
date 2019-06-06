b7e27b60c435c77dd1a895f3f3af908a6e96ec3e
Nick Lee <leexnick@gmail.com>
2019-06-05 21:21:15 -0700

    remove users and tasks contexts

--- src/index.js
+++ src/index.js
@@ -2,19 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import { DashboardProvider } from './contexts/DashboardContext'
-import { UserProvider } from './contexts/UserContext'
-import { TasksProvider } from './contexts/TasksContext'
 import App from './components/App/App'
 import './index.css'
 
 ReactDOM.render(
   <BrowserRouter>
     <DashboardProvider>
-    <UserProvider>
-      <TasksProvider>
-        <App />
-      </TasksProvider>
-    </UserProvider>
+      <App />
     </DashboardProvider>
   </BrowserRouter>,
   document.getElementById('root')
