08e5c1540f68cbf73e9e2c0ef4eb819b60e40c1e
Nick Lee <leexnick@gmail.com>
2019-06-07 12:57:43 -0700

    add landing page intro

--- src/components/LandingPageIntro/LandingPageIntro.js
+++ src/components/LandingPageIntro/LandingPageIntro.js
@@ -1,12 +1,41 @@
 import React, { Component } from 'react'
-import { Section } from '../Utils/Utils';
+import { Section } from '../Utils/Utils'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEdit, faUserPlus, faGem } from '@fortawesome/free-solid-svg-icons'
+import './LandingPageIntro.css'
 
 export default class LandingPageIntro extends Component {
   render() {
     return (
-      <Section className='LandingPageIntro'>
-        
+      <Section className="LandingPageIntro">
+        <div className="LandingPageIntro__add-wrapper">
+          <div className="LandingPageIntro__add">
+            <FontAwesomeIcon icon={faEdit} size="7x" />
+          </div>
+          <h3 className="LandingPageIntro__add-subtext">
+            Add tasks to your BackBurner and customize the rewards and XP you
+            can gain.
+          </h3>
+        </div>
+        <div className="LandingPageIntro__level-up-wrapper">
+          <div className="LandingPageIntro__level-up">
+            <FontAwesomeIcon icon={faUserPlus} size="6x" />
+          </div>
+          <h3 className="LandingPageIntro__level-up-subtext">
+            Check off completed tasks and gain XP and loot rewards.
+          </h3>
+        </div>
+        <div className="LandingPageIntro__reward-wrapper">
+          <div className="LandingPageIntro__reward">
+            <FontAwesomeIcon icon={faGem} size="7x" />
+          </div>
+          <h3 className="LandingPageIntro__reward-subtext">
+            Collect rewards into your own Rewards Chest. Loot your rewards and treat yourself for being productive.
+          </h3>
+        </div>
       </Section>
     )
   }
 }
+
+// Organize all those to-do's on your back-burner and
--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import LandingPageHero from '../../components/LandingPageHero/LandingPageHero';
+import LandingPageIntro from '../../components/LandingPageIntro/LandingPageIntro';
 
 export default class LandingPage extends Component {
   render() {
     return (
       <main>
         <LandingPageHero />
+        <LandingPageIntro />
       </main>
     )
   }
