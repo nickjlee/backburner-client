071ca3258e38aa330e2ad04966399c9b39c07bf7
Nick Lee <leexnick@gmail.com>
2019-06-06 22:36:58 -0700

    styles for rewards chest

--- src/components/RewardItem/RewardItem.js
+++ src/components/RewardItem/RewardItem.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { Button } from '../Utils/Utils'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGem } from '@fortawesome/free-solid-svg-icons'
 import './RewardItem.css'
 
 export default class RewardItem extends Component {
@@ -7,11 +9,18 @@ export default class RewardItem extends Component {
     const { reward, onClaim } = this.props
 
     return (
-      <div key={reward.id} className='RewardItem'>
-        <div className='RewardItem__reward'>{reward.reward}</div>
-        <Button 
-          className='RewardItem__claim-button'
-          onClick={() => onClaim(reward.id)}  
+      <div key={reward.id} className="RewardItem">
+        <div className='RewardItem__reward'>
+          <FontAwesomeIcon
+            className='RewardItem__claim-icon'
+            icon={faGem}
+          />
+          {' '}
+          <div className="RewardItem__reward-text">{reward.reward}</div>
+        </div>
+        <Button
+          className="RewardItem__claim-button"
+          onClick={() => onClaim(reward.id)}
         >
           Claim
         </Button>
--- src/components/RewardsChest/RewardsChest.js
+++ src/components/RewardsChest/RewardsChest.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import RewardItem from '../RewardItem/RewardItem'
 import { Section } from '../Utils/Utils'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCaretDown } from '@fortawesome/free-solid-svg-icons'
+import { faCaretDown, faCaretUp } from '@fortawesome/free-solid-svg-icons'
 import './RewardsChest.css'
 export default class RewardsChest extends Component {
   state ={
@@ -29,13 +29,16 @@ export default class RewardsChest extends Component {
 
   render() {
     const hide = this.state.isHidden ? ' hidden' : ''
+    const icon = this.state.isHidden ? faCaretDown : faCaretUp
 
     return (
       <Section list className='RewardsChest'>
         <h3 onClick={this.toggleHidden}>
           Rewards Chest
           {' '}
-          <FontAwesomeIcon className='blue' icon={faCaretDown} />
+          <FontAwesomeIcon
+            className='blue RewardsChest__icon'
+            icon={icon} />
         </h3>
         <div className={'RewardsChest__rewards' + hide }>
           {this.renderRewards()}
