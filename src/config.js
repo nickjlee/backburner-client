8ab2e369b446fd85e72c22d36a5eb9375dabb1b6
Nick Lee <leexnick@gmail.com>
2019-06-18 20:14:21 -0700

    update server url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://thebackburner-server.herokuapp.com/api',
+  API_ENDPOINT: 'https://backburner-server.herokuapp.com/api',
   TOKEN_KEY: 'backburner-client-token-key'
 }
