b5472f011bbdfde76db910fea526263b40d3bbd0
Nick Lee <leexnick@gmail.com>
2019-06-18 20:54:37 -0700

    add urls to social icons in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGitSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import './Footer.css'
 
 export default class Footer extends Component {
   render() {
     return (
-      <footer className='Footer'>
-        <h5 className='Footer__author'>Created by Nick J. Lee</h5>
-        <div className='Footer__icons'>
-          <FontAwesomeIcon icon={faGitSquare} size='lg'/>
-          <FontAwesomeIcon icon={faLinkedin} size='lg'/>
+      <footer className="Footer">
+        <h5 className="Footer__author">Created by Nick J. Lee</h5>
+        <div className="Footer__icons">
+          <a className='link github-link' href='https://github.com/nickjlee' target='_blank' rel='noreferrer noopener'>
+            <FontAwesomeIcon icon={faGithubSquare} size="lg" />
+          </a>
+          <a className='link linkedin-link' href='https://www.linkedin.com/in/nickjjlee/' target='_blank' rel='noreferrer noopener'>
+            <FontAwesomeIcon icon={faLinkedin} size="lg" />
+          </a>
         </div>
         <h6>Copyright 2019</h6>
         <h6>All Rights Reserved</h6>
