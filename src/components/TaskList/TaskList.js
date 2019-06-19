25a6e82fb2f4500a4c5865b963a35a22fc1f2be7
Nick Lee <leexnick@gmail.com>
2019-06-18 20:55:18 -0700

    add header to tasks list and placeholder texts for when list is empty

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -6,12 +6,28 @@ export default class TaskList extends Component {
   renderTasks() {
     const { taskList = [], onComplete, onRemove } = this.props
 
-    return taskList.map(task => <TaskListItem key={task.id} task={task} onComplete={onComplete} onRemove={onRemove} />)
+    if(taskList.length === 0) {
+      return (
+        <div className='tasklist-no-tasks'>
+          <h3>No tasks... YAY!</h3>
+        </div>
+      )
+    }
+
+    return taskList.map(task => 
+      <TaskListItem
+        key={task.id}
+        task={task}
+        onComplete={onComplete}
+        onRemove={onRemove} 
+      />
+    )
   }
   
   render() {
     return (
       <div className="tasklist-inner">
+        <h2 className='tasklist-title'>Tasks</h2>
         {this.renderTasks()}
       </div>
     )
